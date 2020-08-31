77f590ac4ba6d40b07e0c5333d440f939e07da53
Razvan <razvan.rauta@gmail.com>
2020-08-31 22:37:34 +0300

    Added README.md

--- strappi-admin/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "f7c5e97ed30e4cf09ca79f4d8c5292ab"),
-    },
-  },
-});

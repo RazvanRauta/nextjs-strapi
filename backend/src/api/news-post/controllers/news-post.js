5a858349be9aebb3ca0b15c559a49b46003ccb3d
Razvan <razvan.rauta@gmail.com>
2021-12-10 00:48:50 +0300

    Added news posts

---
+++ backend/src/api/news-post/controllers/news-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  news-post controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::news-post.news-post');
---
+++ backend/src/api/news-post/routes/news-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * news-post router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::news-post.news-post');
